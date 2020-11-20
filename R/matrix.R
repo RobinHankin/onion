4cec6029e23810754b0b046053bd9563bfa6b24d
RobinHankin <hankin.robin@gmail.com>
2020-11-20 15:17:17 +1300

    kickoff for onionic matrices

---
+++ R/matrix.R
@@ -0,0 +1,43 @@
+newonionmat <- function(d,M){
+  if(is.matrix(M)){
+    M[] <- seq_along(M)
+    out <- list(d=d,M=M)
+    class(out) <- "onionmat"
+    return(out)
+  } else {
+    return(d)
+}
+
+isokonionmat <- function(x){
+  with(x,
+       stopifnot(length(x$d) == length(x$M))
+       )
+  return(TRUE)
+}
+
+`onionmat` <- function(x,rows,cols){
+  M <- matrix(TRUE,rows,cols)
+  stopifnot(isokonionmat(x,M))
+  newonionmat(x,M)
+}
+
+`getd` <- function(o){o$d}
+`getm` <- function(o){o$M}
+
+`[.onionmat` <- function(x,...){
+  with(x,
+       return(newonionmat(
+           d[M[...]],
+           M = M[...])))
+}
+
+
+`[<-.onionmat` <- function(x,...,value){
+  with(x,{
+  d[M[...]] <- value
+  return(newonionmat(d,M))
+  }
+  )
+}
+
+
