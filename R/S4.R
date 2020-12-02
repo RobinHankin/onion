d47abd6e9c60f118b430dcb17594faa936b166f5
RobinHankin <hankin.robin@gmail.com>
2020-12-03 10:40:32 +1300

    more work, binary operators + and * work

--- R/S4.R
+++ R/S4.R
@@ -51,6 +51,10 @@ setValidity("octonion", valid_octonion)
 setAs("onion", "matrix", function(from){ from@x} )
 setMethod("as.matrix",signature(x="onion"),function(x){as(x,"matrix")})
 
+setAs("onion", "double", function(from){ as.double(from@x)})
+setMethod("as.double",signature(x="onion"),function(x){as(x,"double")})
+
+
 `quaternion_to_octonion` <- function(from){
   stopifnot(is.quaternion(from))
   as.octonion(rbind(as.matrix(from),matrix(0,4,length(from))))
@@ -423,10 +427,11 @@ setMethod("Arith",signature(e1 = "onion", e2="missing"),
 
 "onion_arith_numeric" <- function(e1,e2){  # e1 onion, e2 numeric
   switch(.Generic,
-         "+" = onion_plus_numeric(e1,  e2),
-         "-" = onion_plus_numeric(e1, -e2),
-         "*" = onion_prod_numeric(e1,  e2),
-         "/" = onion_prod_numeric(e1,1/e2),
+         "+" = onion_plus_numeric (e1,  e2),
+         "-" = onion_plus_numeric (e1, -e2),
+         "*" = onion_prod_numeric (e1,  e2),
+         "/" = onion_prod_numeric (e1,1/e2),
+         "^" = onion_power_numeric(e1,  e2),
          stop(paste("binary operator \"", .Generic, "\" not defined for onions"))
          )
 }
@@ -441,62 +446,123 @@ setMethod("Arith",signature(e1 = "onion", e2="missing"),
          )
 }
 
-
 setMethod("Arith",signature(e1 = "onion"  , e2="onion"  ),   onion_arith_onion  )
 setMethod("Arith",signature(e1 = "onion"  , e2="numeric"),   onion_arith_numeric)
 setMethod("Arith",signature(e1 = "numeric", e2="onion"  ), numeric_arith_onion  )
 
+`harmonize_oo` <- function(a,b){  # a=onion, b=onion; returns a list of 2 matrices
+  sA <- seq_along(a) 
+  sB <- seq_along(b)
+  names(sA) <- names(a)
+  names(sB) <- names(b)
+  ind <- rbind(sA,sB)
+  a <- as.matrix(a)[,ind[1,,drop=TRUE]]
+  b <- as.matrix(b)[,ind[2,,drop=TRUE]]
+  names(a) <- colnames(ind)
+  names(b) <- colnames(ind)
+  return(list(a,b))
+}
+
+`harmonize_on` <- function(a,b){  # a=onion, b=numeric vector; returns a list of matrix, vector
+  sA <- seq_along(a) 
+  sB <- seq_along(b)
+  names(sA) <- names(a)
+  names(sB) <- names(b)
+  ind <-  rbind(sA,sB)
+  a <- as.matrix(a)[,ind[1,,drop=TRUE]]
+  b <- b[ind[2,,drop=TRUE]]  # differs here from harmonize_oo()
+  names(a) <- colnames(ind)
+  names(b) <- colnames(ind)
+  return(list(a,b))
+}
+
 `onion_plus_onion` <- function(a,b){
-  jj <- process(a,b)
+  jj <- harmonize_oo(a,b)
   as.onion(jj[[1]]+jj[[2]])
 }
 
-`onion_plus_numeric`  <- function(a,b){ onion_plus_onion(a,as.onion(b,a)) }
-
-`onion_prod_onion`    <- function(a,b){stop()}
-
-`onion_prod_numeric`  <- function(a,b){  # faster than onion_prod_onion(a,as.onion(b,a))
-  sA <- seq_along(a) 
-  sB <- seq_along(b)
-  names(sA) <- names(a)
-  names(sB) <- names(b)
-  ind <-  rbind(sA,sB)
-
-  out <- as.onion(sweep(as.matrix(a)[,ind[1,,drop=TRUE],drop=FALSE],2,b[ind[2,,drop=TRUE]],"*"))
-  names(out) <- colnames(ind)
-  return(out)
+`onion_plus_numeric`  <- function(a,b){
+  jj <- harmonize_on(a,b)
+  out <- jj[[1]]
+  out[1,] <- out[1,] + jj[[2]]
+  return(as.onion(out))
 }
 
-`onion_power_numeric` <- function(a,b){stop("not implemented")
-  sA <- seq_along(a) 
-  sB <- seq_along(b)
-  names(sA) <- names(a)
-  names(sB) <- names(b)
-  ind <-  rbind(sA,sB)
+`onion_prod_onion` <- function(e1,e2){
+  stopifnot(identical(class(e1),class(e2)))
+  switch(class(a),
+         "quaternion" = quaternion_prod_quaternion(e1,e2),
+         "octonion"   =   octonion_prod_octonion(e1,e2)
+         )
+}
 
-  out <- 
+`octonion_prod_octonion` <- function(o1,o2){
+  stopifnot(is.octonion(o1) & is.octonion(o2))
+  jj <- harmonize_oo(o1,o2)
 
-  out <- as.onion(sweep(as.matrix(a)[,ind[1,,drop=TRUE],drop=FALSE],2,b[ind[2,,drop=TRUE]],"*"))
-  names(out) <- colnames(ind)
-  return(out)
+  out <- .C("octonion_prod",
+           as.double(jj[[1]]),
+           as.double(jj[[2]]),
+           as.integer(length(jj[[1]])),
+           z=as.double(jj[[1]]),
+           PACKAGE="onion"
+           )$z
+  dim(out) <- c(8,length(out)/8)
+  colnames(out) <- colnames(jj[[1]])
+  return(as.octonion(out))
+}
+
+`quaternion_prod_quaternion` <- function(q1,q2){
+  stopifnot(is.quaternion(q1) & is.quaternion(q2))
+  jj <- harmonize_oo(q1,q2)
+
+  out <- .C("quaternion_prod",
+           as.double(jj[[1]]),
+           as.double(jj[[2]]),
+           as.integer(length(jj[[1]])),
+           z=as.double(jj[[1]]),
+           PACKAGE="onion"
+           )$z
+
+  dim(out) <- c(4,length(out)/4)
+  colnames(out) <- colnames(jj[[1]])
+  return(as.quaternion(out))
+}
+
+`onion_prod_numeric`  <- function(a,b){  # faster than onion_prod_onion(a,as.onion(b,a))
+  jj <- harmonize_on(a,b)
+  as.onion(sweep(jj[[1]],2,jj[[2]],"*"))
 }
 
 `onion_power_singlenumber` <- function(o,n){
   stopifnot(length(n)==1)
   stopifnot(n == round(n))
   stopifnot(is.onion(o))
-  if(n>1){
-    return(o*Recall(o,n-1)) 
-  } else if(n==0){
+  if(n==0){
     return(1+o*0)
   } else if(n==1){
     return(o)
   } else if(n<0){
     return(Recall(onion_inverse(o),-n))  
-  } else {
-    stop("this cannot happen")
+  } else { # n>=2
+    out <- o
+    for(i in seq_len(n-1)){  # "-1" because we start at 'o'
+      out <- out*o
+    }
+    return(out)
   }
 }
+
+`onion_power_numeric` <- function(a,b){
+  if(length(b)==1){return(onion_power_singlenumber(a,b))}
+  jj <- harmonize_on(a,b)
+  out <- jj[[1]]
+  for(i in seq_along(out)){
+    out[i] <- onion_power_singlenumber(jj[[1]][i],jj[[2]][i])
+  }
+  return(out)
+}
+
 
 setGeneric("i",function(z){standardGeneric("i")})
 setGeneric("j",function(z){standardGeneric("j")})
