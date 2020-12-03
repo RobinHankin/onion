ebc7e31cb29c1b9540df6ad69bf403a23f51fb9a
RobinHankin <hankin.robin@gmail.com>
2020-12-04 10:34:57 +1300

    unify print methods

--- R/show.R
+++ R/show.R
@@ -1,14 +1,8 @@
-setMethod("show", "quaternion", function(object){quaternion_show(object)})
-`quaternion_show` <- function(x){
+setMethod("show", "onion", function(object){onion_show(object)})
+`onion_show` <- function(x){
   out <- as.matrix(x)
   print(out)
   return(invisible(x))
 }
 
-setMethod("show", "octonion", function(object){quaternion_show(object)})
-`octonion_show` <- function(x){
-  out <- as.matrix(x)
-  print(out)
-  return(invisible(x))
-}
 
