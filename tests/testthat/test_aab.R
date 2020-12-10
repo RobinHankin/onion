d75166d265271210c3bcf979dec08a7f8904bb7d
RobinHankin <hankin.robin@gmail.com>
2020-12-11 07:47:46 +1300

    unified checkerfoo() naming

--- tests/testthat/test_aab.R
+++ tests/testthat/test_aab.R
@@ -1,60 +1,57 @@
-test_that("Test suite aab.R",{
+test_that("Test suite aac.R",{
 
 
-test <- function(x, TOL= 1e-10){
-  stopifnot(Mod(x)<TOL)
-  return(TRUE)
+testequal <- function(x,y){testzero(x-y)}
+testzero <- function(x, TOL= 1e-8){expect_true(max(Mod(x))<TOL)}
+
+n <- 4  # number of times to check
+checkerA <- function(a){  # following are pointwise operations
+  testequal(a,a)
+  testzero(a + (-1)*a)
+  testzero((-1)*a + a)
+  testequal(a+a,2*a)
+  testequal(a+a,a*2)
+  testequal(a+a+a, 3*a)
+  testzero(a+1-a-1)
+  testequal(a/2+a/2,a)
+  testequal(a*a    , a^2)
+  testequal(a*a*a  , a^3) # octonions are *power* associative
+  testequal(a*a*a*a, a^4)
 }
 
-checker1 <- function(a){
-  expect_true(test(a-a))
-  expect_true(test(a + (-1)*a))
-  expect_true(test((-1)*a + a))
-  expect_true(test( (a+a  )-2*a))
-  expect_true(test( (a+a  )-a*2))
-  expect_true(test( (a+a+a)-3*a))
-  expect_true(test( (a+a+a)-a*3))
-  expect_true(test(a+1-a-1))
-  expect_true(test(a+a[1]-a-a[1]))
-  expect_true(test(a/a - 1))
-  expect_true(test(a^2/a - a))
-  expect_true(test(a^3/a^2 - a))
-  expect_true(test(a^4/a^2 - a^2))
-  expect_true(test( (a+a)/a - 2))
-  expect_true(test( (a+a)/(a*a) - 2/a))
-  expect_true(test(a*a       - a^2))
-  expect_true(test(a*a*a     - a^3)) # octonions are *power* associative)
-  expect_true(test(a*a*a*a   - a^4))
-  expect_true(test(1/a - a^(-1)))
-  expect_true(test(1/a^2 - a^(-2)))
-  expect_true(test(1/(a^2) - (1/a)^2))
-  expect_true(test(1/(a^3) - (1/a)^3))
-  expect_true(test( (a/a[1])*a[1] - a))
+checkerB <- function(a,x){
+  testequal(a*x,x*a)
+  testequal(a+x,x+a)
+}
+
+checkerC <- function(a,x,y){
+  testequal(a*(x+y),a*x + a*y)
+  testequal((x+y)*a,a*x + a*y)
+  testequal((x+y)*a,x*a + y*a)
+}
+
+checkerD <- function(a,b){
+  testequal( (a+b) -(b+a))
+  testequal((a/b)*a - b)
+}
+
+
+for(i in 1:10){
+  checkerA(romat(3,4))
+  checkerA(romat(3,5))
+  checkerA(romat(3,4,"octonion"))
+  checkerA(romat(3,5,"octonion"))
+
+  checkerB(romat(3,4),rnorm(1))
+  checkerB(romat(3,4,"octonion"),rnorm(1))
+  checkerB(romat(3,4),rquat(1))
+  checkerB(romat(3,4,"octonion"),roct(1))
+
+  checkerC(romat(3,4),runif(1),runif(1))
+  checkerC(romat(3,4),runif(1),rquat(1))
+  checkerC(romat(3,4),rquat(1),rquat(1))
 }  
 
-for(i in 1:10){
-  checker1(rquat(1))
-  checker1(rquat(9))
-  checker1(roct(1))
-  checker1(roct(9))
-
-}
-
-checker3_assoc <- function(x,y,z){expect_true(test(associator(x,y,z)))}
-checker3_dist <- function(x,y,z){expect_true(test(x*(y+z) - (x*y+x*z)))}
-
-for(i in 1:10){
-  checker3_assoc(rquat(1),rquat(3),rquat(1))
-  checker3_assoc(rquat(3),rquat(3),rquat(1))
-  checker3_assoc(rquat(3),rquat(1),rquat(1))
-  checker3_dist (rquat(1),rquat(3),rquat(1))
-  checker3_dist (rquat(3),rquat(3),rquat(1))
-  checker3_dist (rquat(3),rquat(1),rquat(1))
-
-  checker3_dist (roct(1),roct(3),roct(1))
-  checker3_dist (roct(3),roct(3),roct(1))
-  checker3_dist (roct(3),roct(1),roct(1))
-}
 
 
-})
+} )
