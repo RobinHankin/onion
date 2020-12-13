070c8747db6470b22975f29184a4f6093e3b27ba
RobinHankin <hankin.robin@gmail.com>
2020-12-14 09:05:13 +1300

    coverage for math

--- tests/testthat/test_aal.R
+++ tests/testthat/test_aal.R
@@ -6,21 +6,50 @@ testzero <- function(x, TOL= 1e-8){expect_true(max(Mod(x))<TOL)}
 
 n <- 4  # number of times to check
 checker1  <- function(a){
+    stopifnot(length(a)==1)
     testequal(a,cumsum(a))
     testequal(a,cumprod(a))
 }
 
+
+checker1a <- function(a){
+  testequal(a,asin(sin(a)))
+  testequal(a,atan(tan(a)))
+  testzero(Mod(acos(cos(a))/a)-1)
+
+  testequal(a,asinh(sinh(a)))
+  testequal(a,atanh(tanh(a)))
+  testzero(Mod(acosh(cosh(a))/a)-1)
+
+  testzero(Mod(sign(a))-1)
+
+
+  testequal(log(a,base=exp(1)),log(a))
+  }
+
+
 checker3  <- function(a){
+    stopifnot(length(a) == 3)
     testequal(c(a[1],a[1]*a[2],(a[1]*a[2])*a[3]),cumprod(a))
     testequal(c(a[1],a[1]+a[2],(a[1]+a[2])+a[3]), cumsum(a))
+    testequal(a[1]+a[2]+a[3],sum(a))
+
+    if(is.quaternion(a)){testequal(a[1]*a[2]*a[3],prod(a))}
+    if(is.octonion(a)){expect_error(prod(a))}
+    expect_error(digamma(a))
+
 }
 
 
 for(i in seq_len(n)){
     checker1(rquat(1))
     checker1(roct(1))
+
+    checker1a(rquat(3)/31)
+    checker1a(roct(3)/31)
     checker3(rquat(3))
     checker3(roct(3))
+    
 
 }
 })
