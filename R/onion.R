186cf58f40e6f2fc2df2393aa1cd0590ff431ac6
RobinHankin <hankin.robin@gmail.com>
2020-12-16 10:52:27 +1300

    remove R-based calculation method

--- R/onion.R
+++ R/onion.R
@@ -58,59 +58,6 @@ t(as.matrix(H*as.quaternion(t(cbind(0,x)))/H))[,-1]
 "threeform" <- function(x1,x2,x3){
   Re(x1*(Conj(x2)*x3) - x3*(Conj(x2)*x1))/2
 }
-if(FALSE){
-"R_OprodO" <- function(oct1,oct2){
-  x <- as.matrix(oct1)
-  y <- as.matrix(oct2)
-  out <- x
-  out[1,] = +x[1,]*y[1,] -x[2,]*y[2,] -x[3,]*y[3,] -x[4,]*y[4,] -x[5,]*y[5,] -x[6,]*y[6,] -x[7,]*y[7,] -x[8,]*y[8,]
-  out[2,] = +x[2,]*y[1,] +x[1,]*y[2,] -x[4,]*y[3,] +x[3,]*y[4,] -x[6,]*y[5,] +x[5,]*y[6,] +x[8,]*y[7,] -x[7,]*y[8,]
-  out[3,] = +x[3,]*y[1,] +x[4,]*y[2,] +x[1,]*y[3,] -x[2,]*y[4,] -x[7,]*y[5,] -x[8,]*y[6,] +x[5,]*y[7,] +x[6,]*y[8,]
-  out[4,] = +x[4,]*y[1,] -x[3,]*y[2,] +x[2,]*y[3,] +x[1,]*y[4,] -x[8,]*y[5,] +x[7,]*y[6,] -x[6,]*y[7,] +x[5,]*y[8,]
-  out[5,] = +x[5,]*y[1,] +x[6,]*y[2,] +x[7,]*y[3,] +x[8,]*y[4,] +x[1,]*y[5,] -x[2,]*y[6,] -x[3,]*y[7,] -x[4,]*y[8,]
-  out[6,] = +x[6,]*y[1,] -x[5,]*y[2,] +x[8,]*y[3,] -x[7,]*y[4,] +x[2,]*y[5,] +x[1,]*y[6,] +x[4,]*y[7,] -x[3,]*y[8,]
-  out[7,] = +x[7,]*y[1,] -x[8,]*y[2,] -x[5,]*y[3,] +x[6,]*y[4,] +x[3,]*y[5,] -x[4,]*y[6,] +x[1,]*y[7,] +x[2,]*y[8,]
-  out[8,] = +x[8,]*y[1,] +x[7,]*y[2,] -x[6,]*y[3,] -x[5,]*y[4,] +x[4,]*y[5,] +x[3,]*y[6,] -x[2,]*y[7,] +x[1,]*y[8,]
-  return(as.octonion(out))
-}
-
-"R_HprodH" <- function(quat1,quat2){
-  x <- as.matrix(quat1)
-  y <- as.matrix(quat2)
-  out <- x
-  out[1,] = +x[1,]*y[1,] -x[2,]*y[2,] -x[3,]*y[3,] -x[4,]*y[4,]
-  out[2,] = +x[2,]*y[1,] +x[1,]*y[2,] -x[4,]*y[3,] +x[3,]*y[4,]
-  out[3,] = +x[3,]*y[1,] +x[4,]*y[2,] +x[1,]*y[3,] -x[2,]*y[4,]
-  out[4,] = +x[4,]*y[1,] -x[3,]*y[2,] +x[2,]*y[3,] +x[1,]*y[4,]
-  return(as.quaternion(out))
-}
-
-"print.octonion" <- function(x, h=getOption("horiz"), ...){
-  x <- as.matrix(x)
-  rownames(x) <- c("Re","i","j","k","l","il","jl","kl")
-  if(ncol(x)>0){
-    if(is.null(colnames(x))){colnames(x) <- paste("[",1:ncol(x),"]",sep="")}
-  }
-  if(isTRUE(h)){
-    return(invisible(print(t(x))))
-  } else {
-    return(invisible(print(x)))
-  }
-}
-
-"print.quaternion" <- function(x, h=getOption("horiz"), ...){
-  x <- as.matrix(x)
-  rownames(x) <- c("Re","i","j","k")
-  if(ncol(x)>0){
-    if(is.null(colnames(x))){colnames(x) <- paste("[",1:ncol(x),"]",sep="")}
-  }
-  if(isTRUE(h)){
-    return(invisible(print(t(x))))
-  } else {
-    return(invisible(print(x)))
-  }
-}
-}
 
 setGeneric("plot")
 setMethod("plot","onion",function(x, ...){plot(Re(x),Mod(Im(x)), ...)})
