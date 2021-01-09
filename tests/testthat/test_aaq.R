7b04ebe9927c341c78593484af66e56793910b94
Robin Hankin <hankin.robin@gmail.com>
2021-01-09 13:54:04 +1300

    more tests

--- tests/testthat/test_aaq.R
+++ tests/testthat/test_aaq.R
@@ -14,5 +14,32 @@ test_that("Test suite aaq.R, numeric matrix <op> onion",{
     expect_true(valid_quaternion(rquat()))
     expect_true(valid_octonion(roct()))
 
+
+    expect_true(all(onionmat(Hall,2,3)[1]==Hall))
+    expect_true(all(onionmat(Hall,2,3)[1,drop=TRUE]==Hall))
+    expect_true(all(onionmat(Hall,2,3)[1,drop=FALSE]==Hall))
+    expect_error(all(onionmat(Hall,2,3)[1,,,drop=FALSE]==Hall))
+    expect_true(all(onionmat(Hall,2,3)[1,]==Hall))
+    expect_error(onionmat(Hall,2,3)[1,,]==Hall)
+
+
+    expect_silent(A <- onionmat(Hall,2,3))
+    expect_silent(A[1] <- Him)
+    expect_true(A[1] == Him)
+    
+    expect_silent(A[1,] <- Him)
+    expect_true(all(A[1,] == Him))
+    expect_error(A[1,,] <- Him)
+
+    expect_silent(A[1,] <- 5)
+    expect_true(all(A[1,] ==5))
+    expect_error(A[1,,] <- 5)
+
+    expect_error(A[1,,] <- 3)
+    
+    expect_silent(A[1] <- 3)
+    expect_true(all(A[1] == 3))
+
+
     
 })
