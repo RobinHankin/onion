eb405c18821df12beef6fda3de1884f7a5d3c58a
robin <hankin.robin@gmail.com>
2024-03-29 10:00:26 +0000

    coverage

--- tests/testthat/test_aag.R
+++ tests/testthat/test_aag.R
@@ -17,9 +17,15 @@ test_that("Test suite aag.R, show methods",{
     expect_output(print(rsomat("quaternion")))
     expect_output(print(rsomat("octonion")))
 
+    options("show_onionmats_in_place" = TRUE)
+    expect_output(print(romat("quaternion")))
+    expect_output(print(romat("octonion")))
+
+
     ## restore default:
     options("show_onions_horizontally" = FALSE) 
-    options("show_onions_compactly" = TRUE)
+    options("show_onions_compactly" = FALSE)
+    options("show_onionmats_in_place" = FALSE)
 
  
 })
