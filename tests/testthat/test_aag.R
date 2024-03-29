a8d7edaf8f50f54963759d3cd384372e488f0eb8
robin <hankin.robin@gmail.com>
2024-03-29 08:14:52 +0000

    more coverage

--- tests/testthat/test_aag.R
+++ tests/testthat/test_aag.R
@@ -10,7 +10,13 @@ test_that("Test suite aag.R, show methods",{
     expect_output(print(roct()))
     expect_output(print(romat()))
 
-    options("show_onions_horizontally" = FALSE)
+    options("show_onions_compactly" = TRUE)
+    expect_output(print(rsquat()))
+    expect_output(print(rsoct()))
+
+    ## restore default:
+    options("show_onions_horizontally" = FALSE) 
+    options("show_onions_compactly" = TRUE)
 
  
 })
